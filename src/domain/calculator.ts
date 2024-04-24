e7adf36c9d8fa5b1a674cc57779c171979b87c4a
Aleksey Orekhov <aleoreh@gmail.com>
2024-04-24 16:43:36 +0300

    Реализует функцию calculatePenalty в calculator.ts

--- src/domain/calculator.ts
+++ src/domain/calculator.ts
@@ -1,9 +1,10 @@
+import { dayjs } from "../lib/dayjs"
 import { CalculationResult } from "./calculation-result"
 import { Debt } from "./debt"
-import { Penalty } from "./penalty"
-import { KeyRatePart } from "./key-rate-part"
+import keyRatePart, { KeyRatePart } from "./key-rate-part"
+import { Penalty, PenaltyRow } from "./penalty"
 
-export type CalculatorConfig = {
+type CalculatorConfig = {
     daysToPay: number
     deferredDaysCount: number
     getKeyRate: (date: Date) => number
@@ -12,12 +13,82 @@ export type CalculatorConfig = {
 }
 
 export type CalculatorContext = {
+    config: CalculatorConfig
     calculationDate: Date
+    dueDate: Date
+    debt: Debt
 }
 
-export type CalculatePenalty = (
-    config: CalculatorConfig,
+const doesDefermentActs = (context: CalculatorContext, date: Date): boolean =>
+    dayjs(date).isBefore(
+        dayjs(context.dueDate).add(context.config.deferredDaysCount, "day"),
+        "day"
+    )
+
+const daysOverdue = (dueDate: Date, date: Date): number =>
+    dayjs(date).diff(dueDate, "day")
+
+const calculateDailyAmount = (
     context: CalculatorContext,
-    debt: Debt
-) => Penalty
+    date: Date
+): number => {
+    const k = keyRatePart.getNumericValue(
+        context.config.getKeyRatePart(daysOverdue(context.dueDate, date))
+    )
+    const r = context.config.getKeyRate(date)
+    const s = context.debt.amount
+
+    return doesDefermentActs(context, date) ||
+        context.config.doesMoratoriumActs(date)
+        ? 0
+        : k * r * s
+}
+
+export type CalculatePenalty = (context: CalculatorContext) => Penalty
+export const calculatePenalty: CalculatePenalty = (context) => {
+    // -------- helpers ------- //
+
+    const makeRow = (debtAmount: number, date: Date): PenaltyRow => ({
+        id: date.valueOf(),
+        date: date,
+        debtAmount: debtAmount,
+        doesDefermentActs: doesDefermentActs(context, date),
+        doesMoratiriumActs: context.config.doesMoratoriumActs(date),
+        penaltyAmount: calculateDailyAmount(context, date),
+        rate: context.config.getKeyRate(date),
+        ratePart: context.config.getKeyRatePart(
+            daysOverdue(context.dueDate, date)
+        ),
+    })
+    const nextRow = (row: PenaltyRow): PenaltyRow => {
+        const dayPayment = context.debt.payments
+            .filter((payment) => dayjs(payment.date).isSame(row.date, "day"))
+            .reduce((acc, value) => acc + value.amount, 0)
+
+        const newDebtAmount = row.debtAmount - dayPayment
+        const newDay = dayjs(row.date).add(1, "day").toDate()
+
+        return makeRow(newDebtAmount, newDay)
+    }
+
+    // --------- main --------- //
+
+    const rows: PenaltyRow[] = []
+
+    let curRow: PenaltyRow = makeRow(
+        context.debt.amount,
+        context.calculationDate
+    )
+
+    while (dayjs(curRow.date).isBefore(context.calculationDate)) {
+        rows.push(curRow)
+        curRow = nextRow(curRow)
+    }
+
+    return {
+        period: context.debt.period,
+        rows,
+    }
+}
+
 export type PenaltyToResult = (penalty: Penalty) => CalculationResult
