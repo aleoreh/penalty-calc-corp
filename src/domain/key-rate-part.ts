426b00b5fca34500f0566607c11d5eff92b208d8
Aleksey Orekhov <aleoreh@gmail.com>
2024-04-24 16:42:59 +0300

    Реализует функцию getNumericValue в key-rate-part

--- src/domain/key-rate-part.ts
+++ src/domain/key-rate-part.ts
@@ -2,3 +2,12 @@ export type KeyRatePart = {
     numerator: number
     denominator: number
 }
+
+const getNumericValue = (keyRatePart: KeyRatePart) =>
+    keyRatePart.numerator / keyRatePart.denominator
+
+const keyRatePart = {
+    getNumericValue,
+}
+
+export default keyRatePart
