0f87bf1ccec86cc485c36301c0f07c6e49d53210
Aleksey Orekhov <aleoreh@gmail.com>
2024-04-25 17:11:45 +0300

    Настраивает компонент App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,39 @@
-import "./App.css"
-import { PenaltyCalc } from "./PenaltyCalc"
+import { useEffect, useState } from "react"
 
-function App({ config }: { config: AppConfig }) {
+import { CalculatorConfig } from "../domain/calculator"
+import { RD, remoteData } from "../lib/remote-data"
+import Calculator from "./calculator/Calculator"
+
+import "./App.css"
+
+type AppType = (props: {
+    getConfig: () => Promise<CalculatorConfig>
+}) => JSX.Element
+
+const App: AppType = ({ getConfig }) => {
+    const [config, setConfig] = useState<RD<string, CalculatorConfig>>(
+        remoteData.notAsked()
+    )
+
+    useEffect(() => {
+        setConfig(remoteData.loading())
+        getConfig().then((result) => {
+            setConfig(remoteData.success(result))
+        })
+    }, [getConfig])
+
     return (
         <div className="App">
-            <PenaltyCalc config={config} />
+            {/* <PenaltyCalc config={config} /> */}
+            {remoteData.SRD.match(
+                {
+                    notAsked: () => "Приложение загружено",
+                    loading: () => "Идёт загрузка...",
+                    failure: () => "Настроки не найдены",
+                    success: (value) => <Calculator config={value} />,
+                },
+                config
+            )}
         </div>
     )
 }
