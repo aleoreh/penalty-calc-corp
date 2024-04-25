f430ad02177bc33d43557ada3866bde15c85cb0a
Aleksey Orekhov <aleoreh@gmail.com>
2024-04-25 09:58:52 +0300

    Добавляет плагин для dayjs

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -1,5 +1,8 @@
 import dayjsDef, { Dayjs as DayjsType } from "dayjs"
 import "dayjs/locale"
+import isBetween from "dayjs/plugin/isBetween"
+
+dayjsDef.extend(isBetween)
 
 export const dayjs = dayjsDef
 export type Dayjs = DayjsType
