3d2de053ac8f929490ae8641f97469a3f36c01a4
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-06 14:40:01 +0300

    Добавляет модуль calculator-config.port

---
+++ src/app/calculator-config.port.ts
@@ -0,0 +1,7 @@
+import { CalculatorConfig } from "../domain/calculator-config"
+import calculatorConfigService from "../services/calculator-config-service"
+
+export type GetDefaultCalculatorConfig = () => Promise<CalculatorConfig>
+
+export const getDefaultCalculatorConfig: GetDefaultCalculatorConfig =
+    async () => calculatorConfigService.getConfig()
