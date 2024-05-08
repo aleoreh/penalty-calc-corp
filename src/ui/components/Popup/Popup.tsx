42ccd29b3125229ca9710201fcb820d206ab616f
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-08 14:50:32 +0300

    Подключает useModalClose к Popup

--- src/ui/components/Popup/Popup.tsx
+++ src/ui/components/Popup/Popup.tsx
@@ -1,19 +1,32 @@
 import clsx from "clsx"
 import ReactDOM from "react-dom"
 
+import { useRef } from "react"
+import { useModalClose } from "../../hooks/useModalClose"
 import style from "./Popup.module.css"
 
 const popupElement = document.getElementById("popup")
 
 type PopupProps = {
     isOpened: boolean
+    close: () => void
     children?: JSX.Element | JSX.Element[]
 }
 
-export const Popup = ({ isOpened, children }: PopupProps) => {
+export const Popup = ({ isOpened, close, children }: PopupProps) => {
+    const contentRef = useRef<HTMLDivElement | null>(null)
+
+    useModalClose({
+        isOpened,
+        setClosed: close,
+        containerRef: contentRef,
+    })
+
     return ReactDOM.createPortal(
         <div className={clsx(style.popup, isOpened && style.is_opened)}>
-            <div className={style.content}>{children}</div>
+            <div ref={contentRef} className={style.content}>
+                {children}
+            </div>
         </div>,
         popupElement!
     )
