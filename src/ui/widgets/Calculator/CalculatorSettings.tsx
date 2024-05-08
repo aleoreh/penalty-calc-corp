1e33f216e81f0fe4cd444213e5c670b550362874
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-08 15:16:53 +0300

    WIP: CalculatorSettings

--- src/ui/widgets/Calculator/CalculatorSettings.tsx
+++ src/ui/widgets/Calculator/CalculatorSettings.tsx
@@ -1,54 +1,103 @@
+import "css.gg/icons/css/pen.css"
+import { FormEvent, useState } from "react"
+
+import { CalculatorConfig } from "../../../domain/calculator-config"
 import { dayjs } from "../../../domain/dayjs"
 import { representKeyRateInPercents } from "../../../domain/key-rate"
+import { Form } from "../../components/Form"
+import { Popup } from "../../components/Popup"
 import { UI } from "../../types"
-
 import style from "./Calculator.module.css"
+import Debug from "../../../debug-log.debug"
 
-import "css.gg/icons/css/pen.css"
+type SettingsTableProps = {
+    calculationDate: Date
+    config: CalculatorConfig
+}
 
-export const CalculatorSettings: UI.CalculatorSettings = ({
-    calculationDate,
-    config,
-    setConfig,
-}) => {
+const SettingsTable = ({ config, calculationDate }: SettingsTableProps) => {
     return (
-        <section className={style.calculator_settings}>
-            <table>
-                <caption>Настройки расчета</caption>
-                <tbody>
-                    <tr>
-                        <td>Ключевая ставка на дату расчета</td>
-                        <td>
-                            {representKeyRateInPercents(
-                                config.getKeyRate(calculationDate)
-                            )}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Дней на оплату</td>
-                        <td>{config.daysToPay}</td>
-                    </tr>
-                    <tr>
-                        <td>Дней на отсрочку</td>
-                        <td>{config.deferredDaysCount}</td>
-                    </tr>
-                    <tr>
-                        <td>Действующие моратории</td>
-                        <td>
-                            {config.moratoriums.map(([start, end]) => (
-                                <p>
-                                    {`${dayjs(start).format("L")} - ${dayjs(
-                                        end
-                                    ).format("L")}`}
-                                </p>
-                            ))}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <button title="Редактировать" type="button">
-                <span className="gg-pen"></span>
-            </button>
-        </section>
+        <table>
+            <caption>Настройки расчета</caption>
+            <tbody>
+                <tr>
+                    <td>Ключевая ставка на дату расчета</td>
+                    <td>
+                        {representKeyRateInPercents(
+                            config.getKeyRate(calculationDate)
+                        )}
+                    </td>
+                </tr>
+                <tr>
+                    <td>Дней на оплату</td>
+                    <td>{config.daysToPay}</td>
+                </tr>
+                <tr>
+                    <td>Дней на отсрочку</td>
+                    <td>{config.deferredDaysCount}</td>
+                </tr>
+                <tr>
+                    <td>Действующие моратории</td>
+                    <td>
+                        {config.moratoriums.map(([start, end]) => (
+                            <p>
+                                {`${dayjs(start).format("L")} - ${dayjs(
+                                    end
+                                ).format("L")}`}
+                            </p>
+                        ))}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export const CalculatorSettings: UI.CalculatorSettings = (props) => {
+    const { config, setConfig } = props
+
+    const [editFormOpened, setEditFormOpened] = useState<boolean>(false)
+    const [inputConfig, setInputConfig] = useState<typeof config>(config)
+
+    const handleKeyRateInput = (evt: FormEvent<HTMLInputElement>) => {
+        const keyRate = parseFloat(evt.currentTarget.value) / 100
+        setInputConfig({
+            ...inputConfig,
+            getKeyRate: () => keyRate,
+        })
+    }
+
+    return (
+        <>
+            <section className={style.calculator_settings}>
+                <SettingsTable {...props} />
+                <button
+                    title="Редактировать"
+                    type="button"
+                    onClick={() => setEditFormOpened(true)}
+                >
+                    <span className="gg-pen"></span>
+                </button>
+            </section>
+            <Popup
+                isOpened={editFormOpened}
+                close={() => setEditFormOpened(false)}
+            >
+                <Form
+                    close={() => setEditFormOpened(false)}
+                    reset={() => {}}
+                    submit={{
+                        text: "Сохранить",
+                        fn: () =>
+                            setConfig(Debug.log(inputConfig)(inputConfig)),
+                    }}
+                >
+                    <label>
+                        <span>Ключевая ставка, %</span>
+                        <input name="keyRate" onInput={handleKeyRateInput} />
+                    </label>
+                </Form>
+            </Popup>
+        </>
     )
 }
