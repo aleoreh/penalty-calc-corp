75b8d7d6191fd439697f303acf3600ca010686af
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-19 13:24:34 +0300

    Добавляет диалог подтверждения

---
+++ src/ui/components/ConfirmDialog/ConfirmDialog.tsx
@@ -0,0 +1,68 @@
+import Button from "@mui/material/Button"
+import Dialog from "@mui/material/Dialog"
+import DialogActions from "@mui/material/DialogActions"
+import DialogTitle from "@mui/material/DialogTitle"
+import { useState } from "react"
+
+type ConfirmDialogProps<T> = {
+    id?: string
+    value?: T
+    open: boolean
+    onClose: (value?: T) => void
+    title: string
+    confirmText: string
+}
+
+export const ConfirmDialog = <T,>(props: ConfirmDialogProps<T>) => {
+    const { onClose, value, open, title, confirmText, ...other } = props
+
+    const handleOk = () => {
+        onClose(value)
+    }
+
+    const handleCancel = () => {
+        onClose()
+    }
+
+    return (
+        <Dialog open={open} onClose={handleCancel} {...other}>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogActions>
+                <Button onClick={handleOk}>{confirmText}</Button>
+                <Button autoFocus onClick={handleCancel}>
+                    Отмена
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+type UseConfirmDialogParams<T> = Omit<
+    ConfirmDialogProps<T>,
+    "value" | "title" | "confirmText"
+>
+
+type UseConfirmDialogConfig<T> = Pick<
+    ConfirmDialogProps<T>,
+    "value" | "title" | "confirmText"
+>
+
+type UseConfirmDialogResult<T> = ConfirmDialogProps<T> & {
+    configure: (config: UseConfirmDialogConfig<T>) => void
+}
+
+export const useConfirmDialog = <T,>(
+    params: UseConfirmDialogParams<T>
+): UseConfirmDialogResult<T> => {
+    const [config, setConfig] = useState<UseConfirmDialogConfig<T>>({
+        value: undefined,
+        title: "",
+        confirmText: "OK",
+    })
+
+    return {
+        configure: setConfig,
+        ...params,
+        ...config,
+    }
+}
