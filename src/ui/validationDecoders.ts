b78665101beb691567bb05ab561e981c8380af83
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-22 17:01:35 +0300

    Позволяет ввести число через запятую

--- src/ui/validationDecoders.ts
+++ src/ui/validationDecoders.ts
@@ -3,7 +3,8 @@ import { date, regex, string } from "decoders"
 
 export const inputDecoders = {
     decimal: string
-        .pipe(regex(/^\d*\.?\d*$/, "Ожидается число"))
+        .pipe(regex(/^\d*[.,]?\d*$/, "Ожидается число"))
+        .transform(x => x.replaceAll(",", "."))
         .transform(parseFloat),
     date: string
         .transform((x) => dayjs(x).toDate())
