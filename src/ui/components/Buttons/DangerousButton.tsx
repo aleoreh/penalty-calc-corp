3468c84bac15c67ea527077188de2adebf3125f3
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-22 13:53:16 +0300

    Изменяет непрозрачность DangerousButton

--- src/ui/components/Buttons/DangerousButton.tsx
+++ src/ui/components/Buttons/DangerousButton.tsx
@@ -2,6 +2,6 @@ import Button from "@mui/material/Button"
 import { styled } from "@mui/material/styles"
 
 export const DangerousButton = styled(Button)(() => ({
-    opacity: 0.5,
+    opacity: 0.6,
     textTransform: "lowercase",
 }))
