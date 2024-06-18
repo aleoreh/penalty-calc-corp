3a46814a897562428323d8e509f9edf47ef79173
Aleksey Orekhov <aleoreh@gmail.com>
2024-06-18 09:57:17 +0300

    Добавляет декодер "месяц - год"

--- src/ui/validationDecoders.ts
+++ src/ui/validationDecoders.ts
@@ -4,10 +4,15 @@ import { date, regex, string } from "decoders"
 export const inputDecoders = {
     decimal: string
         .pipe(regex(/^\d*[.,]?\d*$/, "Ожидается число"))
-        .transform(x => x.replaceAll(",", "."))
+        .transform((x) => x.replaceAll(",", "."))
         .transform(parseFloat),
     date: string
         .transform((x) => dayjs(x).toDate())
         .pipe(date)
         .describe("Ожидается дата"),
+    monthYear: string
+        .transform((x) => dayjs(x, 'MMMM YYYY').toDate())
+        .pipe(date)
+        .describe("Ожидается дата в формате MMMM YYYY"),
 }
+
